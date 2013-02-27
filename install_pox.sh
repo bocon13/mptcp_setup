567b180b299deb57b5407dc56613ec94dcd187a2
Brian O'Connor <bocon13@gmail.com>
2013-02-27 09:09:24 +0000

    updated install pox script

--- install_pox.sh
+++ install_pox.sh
@@ -1,4 +1,4 @@
-cd ~/
+cd ~
 
 # Build RipL:
 sudo apt-get install -y python-setuptools
@@ -16,3 +16,7 @@ sudo python setup.py develop
 # Building POX:
 cd ~/
 git clone git://github.com/noxrepo/pox.git
+cd pox
+git checkout master
+
+cd ~
